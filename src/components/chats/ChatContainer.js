4828cf95ee7212e9ad12c6ef870411cdaa7f1953
Baljit Rai <bsrai91@gmail.com>
2017-10-23 15:01:56 +0000

    added, sidebar, and trying to implement messaging, added some css

--- src/components/chats/ChatContainer.js
+++ src/components/chats/ChatContainer.js
@@ -1,19 +1,125 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import SideBar from './SideBar'
+import {COMMUNITY_CHAT, MESSAGE_SENT, MESSAGE_RECIEVED, TYPING} from '../../Events'
+import ChatHeading from './ChatHeading'
+import Messages from '../messages/Messages'
+import MessageInput from '../messages/MessageInput'
+export default class ChatContainer extends Component {
+  constructor(props) {
+    super(props);
 
-// export default class ChatContainer extends Component {
-//   render() {
-//     const { user, logout } = this.props
-//     return (
-//       <div className="container">
-//         <SideBar
-//           logout={logout}
-//           chats={chats}
-//           user={user}
-//           activeChat={activeChat}
-//           setActiveChat={this.setActiveChat}/>
+    this.state = {
+      chats: [],
+      activeChat: null
+    };
+  }
 
-//       </div>
+  componentDidMount() {
+    const { socket } = this.props
+    socket.emit(COMMUNITY_CHAT, this.resetChat)
+  }
 
-//     );
-//   }
-// }
+  //** Reset the chat back to only the chat passed in. **//
+  resetChat = (chat) => {
+    return this.addChat(chat, true)
+  }
+
+  //** Adds chat to the chat container, if reset is true removes all chats and sets that chat to the main chat. Sets the message and typing socket events for that chat. **//
+  //** Adds a message to the specified chat **//
+
+  addChat = (chat, reset) => {
+    const { socket } =this.props
+    const { chats } = this.state
+
+    const newChats = reset ? [chat] : [...chats, chat]
+    this.setState({chats:newChats})
+
+    const messageEvent = `${MESSAGE_RECIEVED}-${chat.id}`
+    const typingEvent = `${TYPING}-${chat.id}`
+
+    socket.on(typingEvent)
+    socket.on(messageEvent, this.addMessageToChat(chat.id))
+
+  }
+
+  //** Returns a function that will add message to chat with the chatId passed in. **//
+  addMessageToChat = (chatId) => {
+    return message => {
+      const { chats } = this.state
+      let newChats = chats.map((chat) => {
+        if(chat.id === chatId)
+          chat.messages.push(message)
+        return chat
+      })
+
+      this.setState({chats:newChats})
+    }
+
+  }
+
+  //** Updates the typing of chat with id passed in. **//
+  updateTypingInChat = (chatId) => {}
+
+  //** Adds a message to the specified chat. **//
+  sendMessage = (chatId, message) => {
+    const { socket } = this.props
+    socket.emit(MESSAGE_SENT, {chatId, message})
+  }
+
+  //** Sends typing status to server **//
+  sendTyping = (chatId, isTyping) => {
+    const { socket } = this.props
+    socket.emit(TYPING, {chatId, isTyping})
+  }
+
+  setActiveChat = (activeChat)=>{
+    this.setState({activeChat})
+  }
+
+  render() {
+    const { user, logout } = this.props
+      return (
+       <div className="container">
+         <SideBar
+           logout={logout}
+           chats={chats}
+           user={user}
+           activeChat={activeChat}
+           setActiveChat={this.setActiveChat}
+           />
+        <div className="chat-room-container">
+          {
+            activeChat !== null ? (
+
+                <div className="chat-room">
+                  <ChatHeading name={activeChat.name} />
+                  <Messages
+                    messages={activeChat.messages}
+                    user={user}
+                    typingUser={activeChat.typingUser}
+                    />
+                  <MessageInput
+                    sendMessage={
+                      (message) => {
+                          this.sendMessage(activeChat.id, message)
+                      }
+                    }
+                    sendTyping={
+                      (isTyping) => {
+                        this.sendTyping(activeChat.id, isTyping)
+                      }
+                    }
+                    />
+                </div>
+
+              ): (
+              <div className="chat-room choose">
+                <h3>Choose a chat!</h3>
+              </div>
+              )
+          }
+        </div>
+        </div>
+    );
+  }
+}
