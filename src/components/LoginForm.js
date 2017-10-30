8cc05ce18b7e0ac954c154f9db6773609f2cc5b4
Unknown <travis.vdm@outlook.com>
2017-10-30 15:23:44 -0600

    changed layout

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { VERIFY_USER } from '../Events';
+import ghost from '../images/ghost.png';
+import icon from '../images/icon.png';
+import lock from '../images/lock.png';
 
 export default class LoginForm extends Component {
   constructor(props) {
@@ -7,8 +10,15 @@ export default class LoginForm extends Component {
 
     this.state = {
       nickname:"",
-      error:""
-
+      error:"",
+      class: {login: "login animated zoomIn",
+              logo: "logo animated zoomIn",
+              username: "input animated zoomIn",
+              password: "input input2 animated zoomIn",
+              button: "btn animated zoomIn",
+              loading: "loading",
+              account: "account animated zoomIn"},
+      visible: true
     };
   }
 
@@ -36,27 +46,49 @@ export default class LoginForm extends Component {
     this.setState({error})
   }
 
+  signInClick() {
+    console.log('OMG THIS WORKS.WOOHOO!!');
+  }
+
+  signUpClick() {
+    this.setState({visible:false})
+  }
+
   render() {
     const { nickname, error } = this.state
     return (
-      <div className="login">
-        <form onSubmit={this.handleSubmit} className="login-form hide-login">
+        <div id="container">
+        <div id="login" className={this.state.class.login}>
+            <div className={this.state.class.logo}>
+                <img src={ghost} />
+            </div>
 
-          <label htmlFor="nickname">
-            <h2>Login</h2>
-          </label>
-          <input
-            ref={(input) => { this.textInput = input }}
-            type="text"
-            id="nickname"
-            value= {nickname}
-            onChange={this.handleChange}
-            placeholder={'UserName'}
-          />
-          <div className="error">{error ? error:null}</div>
+            <form onSubmit={this.handleSubmit}>
+            <div ref={(input) => { this.textInput = input }} onChange={this.handleChange} id="txtEmail" className={this.state.visible ? this.state.class.username : 'input animated zoomOut'}>
+                <img src={icon} />
+                <input className="login_input" autoComplete="off" type="text" name="username" placeholder="Username" />
 
-        </form>
-      </div>
+            </div>
+
+            <div id="txtPassword" className={this.state.visible ? this.state.class.password : 'input input2 animated zoomOut'}>
+
+                <img src={lock} />
+                <input className="login_input" autoComplete="off" type="password" name="username" placeholder="Password" />
+
+            </div>
+
+            <div className="error">{error ? error:null}</div>
+            </form>
+            <button type="button" id="btnLogin" className={this.state.visible ? this.state.class.button : 'btn animated zoomOut'}>Sign in
+            </button>
+            <div id="loading" className={this.state.class.loading}>
+                <div className="dot1"></div>
+                <div className="dot2"></div>
+            </div>
+            <div id="account" className={this.state.visible ? this.state.class.account : "account animated fadeOutDown"}><span>Don't have an account? <a onClick={(e) => this.signUpClick(e)}id="signup" href="#">Sign up</a></span></div>
+        </div>
+    </div>
+
     );
   }
 }
